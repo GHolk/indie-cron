d9cdb55be89902c615babac3ae17b6c9af724d2c
gholk <gholk@disroot.org>
2022-07-24 17:17:38 +0800

    exclude some url from send webmention

--- hook/mastodon-web-mention.js
+++ hook/mastodon-web-mention.js
@@ -2,11 +2,36 @@
 const wmSend = require('send-webmention')
 const cheerio = require('cheerio')
 const {URL} = require('url')
+const fs = require('fs/promises')
 
 // exclude list
 
-class WmCorn {
+class WmCron {
+    constructor(...args) {
+        this._constructor(...args)
+    }
     _constructor() {
+        this.excludeUrlList = []
+    }
+    async excludeUrlLoad(path) {
+        const file = await fs.readFile(path, 'utf8')
+        file.trim().split(/\n/).forEach(line => {
+            if (line.charAt(0) == '#') return
+            if (!line.trim()) return
+            let result = line
+            let scan
+            if (scan = line.match(/^\/(.*)\/([a-z]*)$/)) {
+                result = new RegExp(scan[1], scan[2])
+            }
+            this.excludeUrlList.push(result)
+        })
+    }
+    excludeUrlFilter(list) {
+        return list.filter(u => {
+            const match = this.excludeUrlList.some(m => u.match(m))
+            if (match) console.debug(`exclude ${u}`)
+            return !match
+        })
     }
     htmlToLinkRel(html) {
         const $ = cheerio.load(html)
@@ -30,7 +55,7 @@ class WmCorn {
         const url = status.url
         const html = status.content // content is empty string if reblog
         const linkList = this.htmlToLinkAbs(html, url)
-        for (const link of linkList) {
+        for (const link of this.excludeUrlFilter(linkList)) {
             let result
             console.info(`send ${url} to ${link}`)
             try {
@@ -61,11 +86,19 @@ class WmCorn {
     }
 }
 
-const wmCorn = new WmCorn()
+const wmCron = new WmCron()
 Object.assign(exports, {
-    wmCorn, WmCorn,
+    wmCron, WmCron,
+    async init() {
+        try {
+            await this.wmCron.excludeUrlLoad('webmention-exclude.url')
+        }
+        catch (error) {
+            console.error(error)
+        }
+    },
     async handler(status) {
-        return await this.wmCorn.sendFromToot(status)
+        return await this.wmCron.sendFromToot(status)
     },
     on: 'new-status-post'
 })
